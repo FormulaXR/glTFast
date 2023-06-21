bcc9d5bec0e7ef8d32cb9172bba519bc1f338216
luizcarlosfx <luizcarlos.sfx@gmail.com>
2023-06-21 10:27:47 -0300

    ✨ adds CombineMeshInstantiator

---
+++ Runtime/Scripts/CombinePrimitives/CombineMeshInstantiator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GLTFast;
+using GLTFast.Logging;
+using Unity.Collections;
+using UnityEngine;
+
+namespace GLTFast
+{
+	internal struct PrimitiveData
+	{
+		internal uint NodeIndex { get; set; }
+		internal string MeshName { get; set; }
+		internal Mesh Mesh { get; set; }
+		internal int[] MaterialIndices { get; set; }
+		internal uint[] Joints { get; set; }
+		internal uint? RootJoint { get; set; }
+		internal float[] MorphTargetWeights { get; set; }
+	}
+
+	public class CombineMeshInstantiator : GameObjectInstantiator
+	{
+		private readonly Dictionary<uint, List<PrimitiveData>> _primitives = new Dictionary<uint, List<PrimitiveData>>();
+
+		public CombineMeshInstantiator(IGltfReadable gltf, Transform parent, ICodeLogger logger = null, InstantiationSettings settings = null) : base(gltf, parent, logger,
+			settings)
+		{
+		}
+
+		public bool TryGetNodeObject(uint nodeId, out GameObject node)
+		{
+			return m_Nodes.TryGetValue(nodeId, out node);
+		}
+
+		public override void AddPrimitive(uint nodeIndex, string meshName, Mesh mesh, int[] materialIndices, uint[] joints = null, uint? rootJoint = null,
+			float[] morphTargetWeights = null,
+			int primitiveNumeration = 0)
+		{
+			if ((m_Settings.Mask & ComponentType.Mesh) == 0)
+				return;
+
+			if (!_primitives.TryGetValue(nodeIndex, out List<PrimitiveData> primitives))
+			{
+				primitives = new List<PrimitiveData>();
+				_primitives.Add(nodeIndex, primitives);
+			}
+
+			primitives.Add(new PrimitiveData
+			{
+				NodeIndex = nodeIndex,
+				MeshName = meshName,
+				Mesh = mesh,
+				MaterialIndices = materialIndices,
+				Joints = joints,
+				RootJoint = rootJoint,
+				MorphTargetWeights = morphTargetWeights
+			});
+		}
+
+		//TODO: check how to handle instanced meshes
+		public override void AddPrimitiveInstanced(uint nodeIndex, string meshName, Mesh mesh, int[] materialIndices, uint instanceCount, NativeArray<Vector3>? positions,
+			NativeArray<Quaternion>? rotations,
+			NativeArray<Vector3>? scales, int primitiveNumeration = 0)
+		{
+			base.AddPrimitiveInstanced(nodeIndex, meshName, mesh, materialIndices, instanceCount, positions, rotations, scales, primitiveNumeration);
+		}
+
+		public override void EndScene(uint[] rootNodeIndices)
+		{
+			foreach (uint nodeIndex in _primitives.Keys)
+			{
+				List<PrimitiveData> primitiveList = _primitives[nodeIndex];
+				PrimitiveData first = primitiveList[0];
+
+				if (primitiveList.Count > 1)
+				{
+					var combine = new CombineInstance[primitiveList.Count];
+					var materialIndices = new int[primitiveList.Count];
+					uint[] joints = null;
+					bool hasMorphTargets = first.MorphTargetWeights != null;
+
+					if (first.Joints != null)
+						joints = primitiveList.SelectMany(p => p.Joints).ToArray();
+
+					for (var i = 0; i < primitiveList.Count; i++)
+					{
+						PrimitiveData data = primitiveList[i];
+						Mesh primitiveMesh = data.Mesh;
+						combine[i] = new CombineInstance { mesh = primitiveMesh };
+						materialIndices[i] = data.MaterialIndices[0];
+					}
+
+					var mesh = new Mesh { name = first.MeshName };
+
+					mesh.CombineMeshes(combine, false, false);
+
+					if (hasMorphTargets)
+						CombineMeshUtils.CombineBlendShapes(mesh, primitiveList);
+
+					first.Mesh = mesh;
+					first.MaterialIndices = materialIndices;
+					first.Joints = joints;
+				}
+
+				base.AddPrimitive(nodeIndex, first.MeshName, first.Mesh, first.MaterialIndices, first.Joints, first.RootJoint, first.MorphTargetWeights, 0);
+			}
+
+			base.EndScene(rootNodeIndices);
+		}
+	}
+}
---
+++ Runtime/Scripts/CombinePrimitives/CombineMeshUtils.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GLTFast
+{
+	public class CombineMeshUtils
+	{
+		class BlendShapeFrame
+		{
+			internal Vector3[] Vertices { get; }
+			internal Vector3[] Normals { get; }
+			internal Vector3[] Tangents { get; }
+			int VertexIndex { get; set; }
+			internal float Weight { get; set; }
+			internal int Index { get; }
+			internal string Name { get; }
+
+			internal BlendShapeFrame(string name, int index, int vertexCount)
+			{
+				Name = name;
+				Index = index;
+				Vertices = new Vector3[vertexCount];
+				Normals = new Vector3[vertexCount];
+				Tangents = new Vector3[vertexCount];
+			}
+
+			internal void AddVertices(Vector3[] vertices, Vector3[] normals, Vector3[] tangents)
+			{
+				Array.Copy(vertices, 0, Vertices, VertexIndex, vertices.Length);
+				Array.Copy(normals, 0, Normals, VertexIndex, normals.Length);
+				Array.Copy(tangents, 0, Tangents, VertexIndex, tangents.Length);
+				VertexIndex += vertices.Length;
+			}
+		}
+
+		internal static void CombineBlendShapes(Mesh combinedMesh, List<PrimitiveData> primitiveList)
+		{
+			var frames = new List<BlendShapeFrame>();
+
+			BlendShapeFrame GetBlendShapeFrame(string name, int index)
+			{
+				foreach (BlendShapeFrame frame in frames)
+				{
+					if (frame.Name == name && frame.Index == index)
+						return frame;
+				}
+
+				var newFrame = new BlendShapeFrame(name, index, combinedMesh.vertexCount);
+				frames.Add(newFrame);
+				return newFrame;
+			}
+
+			foreach (PrimitiveData data in primitiveList)
+			{
+				Mesh primitiveMesh = data.Mesh;
+				int vertexCount = primitiveMesh.vertexCount;
+				var vertices = new Vector3[vertexCount];
+				var normals = new Vector3[vertexCount];
+				var tangents = new Vector3[vertexCount];
+
+				for (var shapeIndex = 0; shapeIndex < primitiveMesh.blendShapeCount; shapeIndex++)
+				{
+					string shapeName = primitiveMesh.GetBlendShapeName(shapeIndex);
+
+					for (var frameIndex = 0; frameIndex < primitiveMesh.GetBlendShapeFrameCount(shapeIndex); frameIndex++)
+					{
+						float weight = primitiveMesh.GetBlendShapeFrameWeight(shapeIndex, frameIndex);
+
+						primitiveMesh.GetBlendShapeFrameVertices(shapeIndex, frameIndex, vertices, normals, tangents);
+						BlendShapeFrame frame = GetBlendShapeFrame(shapeName, frameIndex);
+						frame.Weight = weight;
+						frame.AddVertices(vertices, normals, tangents);
+					}
+				}
+			}
+
+			foreach (BlendShapeFrame frame in frames)
+			{
+				combinedMesh.AddBlendShapeFrame(frame.Name, frame.Weight, frame.Vertices, frame.Normals, frame.Tangents);
+			}
+		}
+	}
+}
